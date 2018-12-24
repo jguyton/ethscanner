27d389e23a7ffb70cb00ff7ef9e7cc29c1e00232
JGuyton501 <guyton.justin@gmail.com>
2018-12-24 13:45:53 -0500

    created txn cards

---
+++ src/layouts/components/transactioncard/TransactionCard.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import { Grid, Row, Col } from 'react-bootstrap';
+
+import './txncard.css'
+//components
+
+class TransactionCard extends Component {
+  constructor(props, context) {
+    super(props)
+
+    /* local state variables */
+    this.state = {
+    }
+
+
+  }
+
+  render() {
+    return(
+      <div>
+        <Grid>
+          <div className="transactionCard">
+            <Row>
+              <Col xs={12} md={12} lg={12}>
+                <p>Hash: <a href={'https://etherscan.io/tx/' + this.props.hash} target='_blank' rel="noopener noreferrer"> {this.props.hash}</a></p>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12} md={5} lg={5}>
+                <p>Block: {this.props.blockNumber}</p>
+              </Col>
+              <Col xs={12} md={5} lg={5}>
+                <p>Time Stamp: {this.props.timeStamp}</p>
+              </Col>
+              <Col xs={12} md={2} lg={2}>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12} md={5} lg={5}>
+                <p>From: &nbsp;
+                  <a href={"/address/" + this.props.from}>
+                    {this.props.from}
+                  </a>
+                </p>
+              </Col>
+              <Col xs={12} md={5} lg={5}>
+                <p>To: &nbsp;
+                  <a href={"/address/" + this.props.to}>
+                    {this.props.to}
+                  </a>
+                </p>
+              </Col>
+              <Col xs={12} md={2} lg={2}>
+                <p>Value: {this.props.value / (10**18)} ETH</p>
+              </Col>
+            </Row>
+          </div>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default TransactionCard
+
+/*
+
+*/
--- src/layouts/views/Dashboard/Dashboard.js
+++ src/layouts/views/Dashboard/Dashboard.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import API from '../../lib/api';
+import {withRouter } from 'react-router-dom';
 
 //components
 import Wrapper from '../../components/wrapper/Wrapper'
+import TransactionCard from '../../components/transactioncard/TransactionCard'
 class Dashboard extends Component {
 
   constructor(props, context) {
@@ -16,6 +18,10 @@ class Dashboard extends Component {
       offset: 10,
       sort: "asc"
     };
+
+
+    this.updateSearch = this.updateSearch.bind(this);
+    this.search = this.search.bind(this);
   }
 
   componentDidMount() {
@@ -24,23 +30,59 @@ class Dashboard extends Component {
     ]).then(results => {
 
       const transactions = results[0].result;
-      console.log(transactions);
+
       this.setState({
         transactions
       });
     });
   }
 
+  updateSearch(event) {
+    this.setState({search: event.target.value});
+  }
+
+  search() {
+    const search = this.state.search;
+    this.setState({search});
+
+    // check if search has basic requirements for account
+    if (/^(0x)?[0-9a-f]{40}$/i.test(search)) {
+      this.props.history.push(this.state.search);
+      this.componentDidMount();
+    } else {
+      alert("Yoooo fam, that's an incorrect address")
+    }
+  }
 
   render() {
     return (
       <Wrapper>
         <div>
-          GANG
+          <div className="inputbox" style={{textAlign: 'center', margin: '5% 0 5% 0'}}>
+          <input type="text"  onChange={this.updateSearch} placeholder="enter ethereum address or block number" name="address"/>
+          <button onClick={this.search}>
+            <div>go</div>
+          </button>
+          </div>
+
+          {this.state.transactions.map((transaction, index) => {
+
+          return [
+            <TransactionCard
+              key={index}
+              hash={transaction.hash}
+              blockNumber={transaction.blockNumber}
+              timeStamp={transaction.timeStamp}
+              from={transaction.from}
+              to={transaction.to}
+              value={transaction.value}
+            />
+          ]
+          })}
         </div>
       </Wrapper>
     )
   }
 }
 
-export default Dashboard
+export default withRouter(Dashboard)
