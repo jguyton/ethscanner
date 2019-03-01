38b6a4864d6bc2450fb4f0f699d0db6ab08ce8ac
Justin <11680677+jguyton@users.noreply.github.com>
2019-02-28 20:56:16 -0500

    Update config.js
    oooooooo

--- src/layouts/lib/config.js
+++ src/layouts/lib/config.js
@@ -1,5 +1,5 @@
 let config = {};
 
 config.ETHSCAN_API_URL="https://api.etherscan.io/api?module=account&action=txlist";
-config.ETHSCAN_API_KEY = "9R3G4VQIXWBXZ9TXXDKC54DQ3FAT9JAA83"
+config.ETHSCAN_API_KEY = ""
 module.exports = config
